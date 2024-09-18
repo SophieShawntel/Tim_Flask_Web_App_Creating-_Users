3bf43649e7990bf3b9e82bc02382423600122af2
SophieShawntel <boundtomakeit210@gmail.com>
2024-09-18 08:30:08 -0700

    update

--- website/models.py
+++ website/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(50000))
+    data = db.Column(db.String(10000))
     date = db.Column(db.DateTime, default=datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
